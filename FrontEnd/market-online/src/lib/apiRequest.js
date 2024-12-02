74df152e077a811e38f85d0bb09ed4e7ac00e2ea
thutrang1423 <96735160+thutrang1423@users.noreply.github.com>
2024-12-02 19:24:46 +0700

    Update apiRequest.js

--- FrontEnd/market-online/src/lib/apiRequest.js
+++ FrontEnd/market-online/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8800/api",
+    baseURL:"https://signup-loginbe-2.onrender.com",
     withCredentials: true,
 });
 export default apiRequest;
